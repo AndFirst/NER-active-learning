818f6be87250ee8024b7c0d02a93f22615bfbd38
iokninsk <irekokninski12370@gmail.com>
2024-04-02 10:48:31 +0200

    Create simple NER model and data processing.

---
+++ ner_playground/main.py
@@ -0,0 +1,7 @@
+from data_processing import create_unlabeled_dataset, create_word_to_idx, create_tag_to_idx
+
+if __name__ == '__main__':
+    create_unlabeled_dataset('dataset/ner_dataset.csv')
+    # create_word_to_idx('dataset/unlabeled.csv',
+    #                    'dataset/word2idx.json')
+    # create_tag_to_idx(['per', 'geo'], 'dataset/tag2ixd.json')
