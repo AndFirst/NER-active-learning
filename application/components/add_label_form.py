2540e84841ada54862bfc71e39e662b2e5d6230e
iokninsk <irekokninski12370@gmail.com>
2024-04-11 01:30:06 +0200

    Feature: add labels create color wheel implement checking correctness of labels

--- application/components/add_label_form.py
+++ application/components/add_label_form.py
@@ -10,22 +10,40 @@ from kivy.properties import ObjectProperty
 kv_string = '''
 <AddLabelForm>:
     orientation: 'vertical'
+    padding: 20
+    canvas.before:
+        Color:
+            rgba: 0, 0, 0, 1
+        Line:
+            rectangle: (self.x, self.y, self.width, self.height)
+            width: 1
     ScrollView:
+        id: scroll_view    
         scroll_type: ['bars']
+        size_hint: 1, 1
         bar_width: 20
+        scroll_y: 1.0
         # canvas.before:
         #     Color:
-        #         rgba: 0, 0, 0, 1  # Czarny kolor
-        #     Rectangle:
-        #         pos: self.pos
-        #         size: self.size
+        #         rgba: 0, 1, 0, 1
+        #     Line:
+        #         rectangle: (self.x, self.y, self.width, self.height)
+        #         width: 1
         GridLayout:
-            padding: 50
-            spacing: 10
+            id: layout
+            spacing: 20
             cols: 1
             size_hint_y: None
             height: self.minimum_height
+            # canvas.before:
+            #     Color:
+            #         rgba: 1, 0, 0, 1
+            #     Line:
+            #         rectangle: (self.x, self.y, self.width, self.height)
+            #         width: 1
             LabelRow:
+
+
 '''
 
 # Wczytanie kodu Kivy z stringa
@@ -33,4 +51,7 @@ Builder.load_string(kv_string)
 
 
 class AddLabelForm(BoxLayout):
-    pass
+    @property
+    def label_rows(self):
+        label_rows = [child for child in self.ids.layout.children if isinstance(child, LabelRow)]
+        return label_rows
--- application/data_types.py
+++ application/data_types.py
@@ -1,12 +1,22 @@
 from dataclasses import dataclass
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Any
 
 
 @dataclass
 class LabelData:
     label: str
-    idx: int
     color: Tuple[int, int, int, int]
+
+    def is_empty(self) -> bool:
+        return not self.label.strip()
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, LabelData):
+            return False
+        return self.label.lower() == other.label.lower()
+
+    def __hash__(self) -> int:
+        return hash(self.label.lower())
 
 
 @dataclass
