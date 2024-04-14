fda6610e88ba288e6eb5e67f4881127cee5a2e81
zdamszel <01169157@pw.edu.pl>
2024-04-14 23:34:33 +0200

    frontend improvements

--- app/screens/summary_screen.py
+++ app/screens/summary_screen.py
@@ -2,39 +2,32 @@ from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
-
-from kivy.uix.gridlayout import GridLayout  # Import GridLayout
-
+from kivy.uix.gridlayout import GridLayout 
 from file_operations import save_project
 
 kv_string = """
 <SummaryScreen>:
     BoxLayout:
         id: box_layout
+        color: (0, 0, 0, 1)  
         orientation: 'vertical'
         Label:
-            text: "Podsumowanie projektu:"
+            text: "Summary"
             font_size: 24  
-            color: (0, 0, 0, 1)  
             halign: 'center'  
             valign: 'top'
         GridLayout:  
             cols: 2
             Label:
-                text: "Pole"  
+                text: "Field"  
                 font_size: 20 
                 bold: True 
                 halign: 'left'
             Label:
-                text: "Wartość"  
+                text: "Value"  
                 font_size: 20 
                 bold: True 
                 halign: 'right'  
-        BoxLayout:
-            size_hint_y: 0.8
-            orientation: 'vertical'
-            Label:
-                text: 'Summary:'
         PrevNextButtons:
             id: prev_next_buttons
             size_hint_y: 0.2
@@ -52,15 +45,10 @@ class SummaryScreen(Screen):
         self.ids.prev_next_buttons.on_back = self.go_to_add_labels
         self.ids.prev_next_buttons.on_next = self.go_to_main_menu
 
-        # Tworzenie etykiety do wyświetlania shared_data
         data_label = Label(text=str(self.shared_data), size_hint_y=None)
         data_label.bind(width=lambda s, w: s.setter("text_size")(s, (w, None)))
-
-        # Dodanie etykiety do ScrollView, aby umożliwić przewijanie, jeśli dane są zbyt duże
         scroll_view = ScrollView()
         scroll_view.add_widget(data_label)
-
-        # Dodanie ScrollView do BoxLayout
         self.ids.box_layout.add_widget(scroll_view)
 
     def go_to_add_labels(self):
@@ -72,9 +60,36 @@ class SummaryScreen(Screen):
 
     def on_enter(self):
         grid_layout = self.ids.box_layout.children[1]
+        
+        # Usuwamy wszystkie istniejące widgety z GridLayout
+        grid_layout.clear_widgets()
 
-        for field, value in self.shared_data.get_dict().items():
-            field_label = Label(text=field, halign="left")
-            value_label = Label(text=value, halign="right")
+        # Lista etykiet pól
+        field_labels = ["Name", "Description", "Save Path", "Dataset Path"]
+
+        # Pobieramy wartości z obiektu ProjectData
+        values = [
+            self.shared_data.name,
+            self.shared_data.description,
+            self.shared_data.save_path,
+            self.shared_data.dataset_path
+        ]
+
+        # Dodajemy labelki dla pól i wartości
+        for field_label_text, value in zip(field_labels, values):
+            field_label = Label(text=field_label_text, halign="left")
+            value_label = Label(text=str(value), halign="left")
             grid_layout.add_widget(field_label)
             grid_layout.add_widget(value_label)
+
+        # Dodajemy labelki dla etykiet
+        label_title = Label(text="Labels", font_size=20, bold=True, halign="left")
+        grid_layout.add_widget(label_title)
+        for label_data in self.shared_data.labels:
+            label_name = label_data.label
+            label_color = label_data.color
+            label_info = f"Name: {label_name}, Color: {label_color}"
+            label_info_label = Label(text=label_info, halign="left")
+            grid_layout.add_widget(label_info_label)
+
+
